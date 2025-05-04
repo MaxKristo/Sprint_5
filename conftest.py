ae0377e196a2ce7509ab064d68d2266bb3263c14
Maxim Shavel <maksimka411@yandex.ru>
2025-05-04 05:10:54 +0300

    Create README.md

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+from data import Data
+
+
+@pytest.fixture(scope='function')
+def driver():
+    driver = webdriver.Chrome()                       # создали объект для опций
+    driver.set_window_size(1920, 1080)   # задали размер окна
+    driver.get(Data.Stellar_Burgers_URL)              # запуск страницы Stellar Burgers
+    return driver
+    driver.quit()
---
+++ data.py
@@ -0,0 +1,10 @@
+import random
+
+class Data:
+    Stellar_Burgers_URL = "https://stellarburgers.nomoreparties.site/"
+    email = "max_shavel_19@yandex.ru"
+    password = "park7700"
+    username = "Maxim"
+
+    random_email = f"max_shavel_19_{random.randint(111, 999)}@yandex.ru"
+    random_password = f"{random.randint(111111, 999999)}"
