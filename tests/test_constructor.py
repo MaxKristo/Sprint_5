bbf57aa210fd973d33b8ccb77e44b3ebe0a86a2e
Maxim Shavel <maksimka411@yandex.ru>
2025-05-04 05:26:43 +0300

    файл проверки переходов к разделам в разделе «Конструктор»

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from locators import Locators
 from conftest import driver
 
+
 class TestConstructor:
 
     def test_go_to_sousy(self, driver):
@@ -15,7 +16,6 @@ class TestConstructor:
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.fillings_block))
         driver.find_element(*Locators.fillings_block).click()
         assert driver.find_element(*Locators.proverka_fillings)
-
 
     def test_go_to_buns(self, driver):
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(Locators.sauces_block))
